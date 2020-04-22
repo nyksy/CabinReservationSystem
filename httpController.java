50b0dcfc46b1e60511580636514f6ebb8b1ccf50
Mousse3 <tagrohn@student.uef.fi>
2020-04-22 14:27:19 +0300

    Testikommentti

--- httpController.java
+++ httpController.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import org.json.*;
 
 public class httpController {
-
+    //Testikommentti
     public static void main(String[] args) {
         try {
             getHTTP();
