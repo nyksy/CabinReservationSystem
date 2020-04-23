7aa9c7ba4ac871e4c53b1896f88ec029fe99c7be
Mousse3 <tagrohn@student.uef.fi>
2020-04-23 12:24:07 +0300

    JsonDecompiler testausta vol 3.

--- JsonDecompiler.java
+++ JsonDecompiler.java
@@ -1,29 +1,39 @@
 import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Luokka rajapinnan JSON vastauksen dekoodaamiseen ja palauttamiseen ArrayList<ArrayList<String>>
+ * muodossa
+ * <p>
+ * Dependency: json-20190722.jar
+ */
 public class JsonDecompiler {
 
-    public void decompile(String json) {
+    /**
+     * Metodi json viestin purkamiseksi ja tallettamiseksi ArrayList<String[]> muotoon
+     * Ulkoinen ArrayList sisältää String[] taulukkoja, jotka kuvastavat tietokannan rivejä.
+     * Sisäinen String[] sisältää tietokannasta saadun rivin tiedot attribuutti per indeksi.
+     *
+     * @param json JSON viesti, "Array of Arrays" - muodossa
+     */
+    public ArrayList<String[]> decompile(String json) {
         JSONArray mJsonArray = new JSONArray(json);
-        ArrayList<ArrayList<String>> PalautusLista = new ArrayList<>();
-        ArrayList<String> Valilista = new ArrayList<>();
-        System.out.println(mJsonArray.length());
-        System.out.println(mJsonArray.getJSONArray(0).length());
-        for(int i = 0; i < mJsonArray.length(); i++){
-            for(int j = 0; j < mJsonArray.getJSONArray(i).length(); j++) {
-                Valilista.add(mJsonArray.getJSONArray(i).get(j).toString());
+        ArrayList<String[]> returnList = new ArrayList<>();
+        for (int i = 0; i < mJsonArray.length(); i++) {
+            String[] tempArray = new String[mJsonArray.getJSONArray(i).length()];
+            for (int j = 0; j < mJsonArray.getJSONArray(i).length(); j++) {
+                tempArray[j] = mJsonArray.getJSONArray(i).get(j).toString();
             }
+            returnList.add(tempArray);
         }
-        PalautusLista.add(Valilista);
 
-        System.out.println("Palautuslistan koko " + PalautusLista.size());
-        for (int i = 0; i < PalautusLista.size(); i++) {
-            for (int j = 0; j < PalautusLista.get(i).size(); j++) {
-                System.out.println(PalautusLista.get(i).get(j));
+        System.out.println("Palautuslistan koko " + returnList.size());
+        for (int i = 0; i < returnList.size(); i++) {
+            for (int j = 0; j < returnList.get(i).length; j++) {
+                String[] temp = returnList.get(i);
+                System.out.println(temp[j]);
             }
         }
+        return returnList;
     }
 }
